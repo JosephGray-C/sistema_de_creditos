bf3d0d9f46f80384b8ddd667cbb3aaa54c803bee
Usuario <Usuario@PC-Sebastian>
2024-01-25 23:08:52 -0600

    Carga inicial 2

--- src/main/java/com/mycompany/reclutador/Reclutador.java
+++ src/main/java/com/mycompany/reclutador/Reclutador.java
@@ -11,6 +11,6 @@ package com.mycompany.reclutador;
 public class Reclutador {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Git");
     }
 }
